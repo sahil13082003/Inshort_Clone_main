e0327df4e5777e1a87d90a56964f2d0df0b3893f
sahil13082003 <sahilgolhar7@gmail.com>
2024-11-01 08:43:19 +0530

    resolve error

--- client/src/components/Articles.jsx
+++ client/src/components/Articles.jsx
@@ -1,6 +1,5 @@
 import { useState, useEffect } from "react";
 import { getNews } from "../service/api";
-
 import InfiniteScroll from "react-infinite-scroll-component";
 
 //components
@@ -10,10 +9,10 @@ const Articles = () => {
   const [news, setNews] = useState([]);
   const [page, setPage] = useState(0);
 
-  // load all data filled with article as soon as website load and as soon as scroll reached to end
+  // load all data filled with articles as soon as website loads or scroll reaches the end
   useEffect(() => {
     const dailyNews = async () => {
-      const response = await getNews();
+      const response = await getNews() || []; // Ensure response is an array
       console.log(new Set([...news, ...response]));
       setNews([...new Set([...news, ...response])]);
     };
@@ -30,19 +29,11 @@ const Articles = () => {
       next={() => setPage((page) => page + 1)}
       hasMore={true}
     >
-      {" "}
-      {news.map((article) => (
-        <Article article={article} />
-      ))}{" "}
+      {news.map((article, index) => (
+        <Article key={index} article={article} />
+      ))}
     </InfiniteScroll>
   );
 };
 
 export default Articles;
-/**
- * Looping the article using map(()=>{
- *
- * Single Component with data
- *
- * })=?
- */
